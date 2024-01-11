7bab64dbc5937312ff971782e9c09de12c4c10b1
bfddeveloper <bdicksondev@gmail.com>
2024-01-11 09:48:15 -0600

    Initial Commit

---
+++ puzzle 15 AI/puzzle_15_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  puzzle_15_AIApp.swift
+//  puzzle 15 AI
+//
+//  Created by Brody Dickson on 1/11/24.
+//
+
+import SwiftUI
+
+@main
+struct puzzle_15_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ puzzle 15 AIUITests/puzzle_15_AIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  puzzle_15_AIUITestsLaunchTests.swift
+//  puzzle 15 AIUITests
+//
+//  Created by Brody Dickson on 1/11/24.
+//
+
+import XCTest
+
+final class puzzle_15_AIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
